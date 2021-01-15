dd3c3f53e46c83cf3c6fc9f0e452102f5cc0a3ca
Isaiah Smith <jordankid93@gmail.com>
2021-01-15 14:51:15 -0800

    Blog: 1/15/2021- The Adventure of building a SaaS Business Begins (#20)
    * Blog: 1/15/2021- The Adventure of building a SaaS Business Begins
    
    * chore: update snapshots (new year)

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,8 @@ module.exports = {
           {
             resolve: "gatsby-remark-images",
             options: {
-              maxWidth: 590,
+              maxWidth: 1200,
+              sizeByPixelDensity: true,
             },
           },
           {
