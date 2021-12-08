919a76f91043a734361b18d15ae4f542575d3912
Isaiah Smith <jordankid93@gmail.com>
2021-12-07 18:12:44 -0800

    chore: add next config
    - dont know if i actually need this but it seems like I might?

---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  eslint: {
+    // REF: https://nextjs.org/docs/basic-features/eslint#linting-custom-directories-and-files
+    dirs: ["pages", "components", "lib", "hooks"],
+  },
+}
