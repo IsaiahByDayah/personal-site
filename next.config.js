fd52994af5260df96523021be4d2d551bd545823
Isaiah Smith <jordankid93@gmail.com>
2021-12-08 00:30:02 -0800

    chore: global redirects

--- next.config.js
+++ next.config.js
@@ -3,4 +3,31 @@ module.exports = {
     // REF: https://nextjs.org/docs/basic-features/eslint#linting-custom-directories-and-files
     dirs: ["pages", "components", "lib", "hooks"],
   },
+  redirects: async () => [
+    {
+      source: "/twitter",
+      destination: "https://twitter.com/IsaiahByDayah",
+      permanent: false,
+    },
+    {
+      source: "/twitch",
+      destination: "https://www.twitch.tv/isaiahbydayah",
+      permanent: false,
+    },
+    {
+      source: "/youtube",
+      destination: "https://www.youtube.com/isaiahsmith",
+      permanent: false,
+    },
+    {
+      source: "/patreon",
+      destination: "https://www.patreon.com/isaiahbydayah",
+      permanent: false,
+    },
+    {
+      source: "/github",
+      destination: "https://github.com/IsaiahByDayah",
+      permanent: false,
+    },
+  ],
 }
