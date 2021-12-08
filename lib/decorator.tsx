d752d4aef9fa4de0f915980ae3ed786df2b02075
Isaiah Smith <jordankid93@gmail.com>
2021-12-07 18:12:23 -0800

    chore: fix decorator
    - just remove extra types

--- lib/decorator.tsx
+++ lib/decorator.tsx
@@ -1,8 +1,10 @@
 import { ComponentType, ReactNode } from "react"
-import { DecoratorFunction, StoryContext } from "@storybook/addons"
+import { DecoratorFunction } from "@storybook/addons"
 
-const decorator = (Component: ComponentType<StoryContext>): DecoratorFunction<ReactNode> => {
-  const DecoratorFunc: DecoratorFunction<ReactNode> = (story, context) => <Component {...context}>{story()}</Component>
+const decorator = (Component: ComponentType): DecoratorFunction<ReactNode> => {
+  const DecoratorFunc: DecoratorFunction<ReactNode> = (story, context) => (
+    <Component {...context}>{story()}</Component>
+  )
   return DecoratorFunc
 }
 
