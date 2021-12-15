68ccc0eae1715a65199c3645ed7e18b5c0b0f8be
Isaiah Smith <jordankid93@gmail.com>
2021-12-15 14:49:00 -0800

    Update Header.stories.tsx

--- components/scaffold/Header.stories.tsx
+++ components/scaffold/Header.stories.tsx
@@ -4,6 +4,7 @@ import Header from "components/scaffold/Header"
 
 export default {
   title: "Scaffold/Header",
+  component: Header,
 } as Meta
 
 export const Basic: Story = (args) => <Header {...args} />
