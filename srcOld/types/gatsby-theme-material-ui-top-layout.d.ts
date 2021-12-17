69c7a6009deaa6380fe7abf24951fb7c580f17d4
Isaiah Smith <jordankid93@gmail.com>
2021-12-16 22:49:09 -0800

    chore: remove old src files

--- srcOld/types/gatsby-theme-material-ui-top-layout.d.ts
+++
@@ -1,1 +0,0 @@
-declare module "gatsby-theme-material-ui-top-layout/src/components/top-layout"
--- srcOld/utils/decorator.tsx
+++
@@ -1,7 +0,0 @@
-import { ComponentType, ReactNode } from "react"
-import { DecoratorFunction, StoryContext } from "@storybook/addons"
-
-export const decorator =
-  (Component: ComponentType<StoryContext>): DecoratorFunction<ReactNode> =>
-  (story, context) =>
-    <Component {...context}>{story()}</Component>
