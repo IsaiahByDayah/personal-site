21a6283e85693e2e9ce6cac53e7cd0443d53c915
Isaiah Smith <jordankid93@gmail.com>
2023-03-26 02:19:57 -0400

    fix: cleanup manual types

--- lib/prismic/types/ProjectDocument.ts
+++
@@ -1,7 +0,0 @@
-import { PrismicDocument } from "@prismicio/types"
-
-import ProjectMockData from ".slicemachine/assets/customtypes/project/mocks.json"
-
-type ProjectDocument = PrismicDocument<typeof ProjectMockData["data"]>
-
-export default ProjectDocument
--- lib/prismic/types/TagDocument.ts
+++
@@ -1,7 +0,0 @@
-import { PrismicDocument } from "@prismicio/types"
-
-import TagMockData from ".slicemachine/assets/customtypes/tag/mocks.json"
-
-type TagDocument = PrismicDocument<typeof TagMockData["data"]>
-
-export default TagDocument
