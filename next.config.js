77b64e9de707ddf1770006b1e55c67856ac8fb3f
Isaiah Smith <jordankid93@gmail.com>
2023-03-29 22:28:53 -0700

    fix: remove trailing slashes (#43)

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  trailingSlash: true,
   eslint: {
     // REF: https://nextjs.org/docs/basic-features/eslint#linting-custom-directories-and-files
     dirs: ["pages", "components", "lib", "hooks"],
