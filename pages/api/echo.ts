c75346f6f0736d5289f9659df4ca52d507b04721
Isaiah Smith <jordankid93@gmail.com>
2023-03-29 23:42:13 -0700

    chore: add echo endpoint

---
+++ pages/api/echo.ts
@@ -0,0 +1,19 @@
+// REF: https://prismic.io/blog/nextjs-sites-on-demand-isr
+
+import { NextApiHandler } from "next"
+
+// Import your app's Link Resolver (if your app uses one)
+
+/**
+ * This API endpoint will be called by a Prismic webhook. The webhook
+ * will send an object containing a list of added, updated, or deleted
+ * documents. Pages for those documents will be rebuilt.
+ *
+ * The Prismic webhook must send the correct secret.
+ */
+const handler: NextApiHandler = async (req, res) => {
+  console.log("[DEBUG] echo:", req)
+  return res.json(req.body)
+}
+
+export default handler
