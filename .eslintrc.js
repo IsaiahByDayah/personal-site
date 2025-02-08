8af58c86ff8866eacf6c056080ab6355c57e84c3
Isaiah Smith <jordankid93@gmail.com>
2025-02-08 09:19:18 -0800

    chore: clean eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,5 @@ module.exports = {
   extends: ["next/core-web-vitals", "prettier"],
   rules: {
     semi: ["error", "never"],
-    "@next/next/no-img-element": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      {
-        ignoreRestSiblings: true,
-      },
-    ],
   },
 }
