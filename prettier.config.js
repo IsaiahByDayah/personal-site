7e5be86df5194f2e3d38e7ff62d1639b5d5a5ee5
Isaiah Smith <jordankid93@gmail.com>
2025-02-07 19:15:48 -0800

    chore: update prettier config

---
+++ prettier.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  semi: false,
+  singleQuote: false,
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    // "prettier-plugin-tailwindcss",
+  ],
+  importOrder: [
+    "",
+    "<BUILTIN_MODULES>", // Node.js built-in modules
+    "",
+    "<THIRD_PARTY_MODULES>", // Imports not matched by other special words or groups.
+    "",
+    "^(app|components|hooks|lib|pages|slices)(/.*)$",
+    "^slices$",
+    "",
+    "^[.]", // relative imports
+  ],
+  tailwindConfig: "./tailwind.config.ts",
+  tailwindFunctions: ["clsx"],
+}
