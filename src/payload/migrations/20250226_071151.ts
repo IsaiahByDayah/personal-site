1781ab3eba57fcbd79bca7691e80f4584c54f697
Isaiah Smith <jordankid93@gmail.com>
2025-02-25 23:26:51 -0800

    chore: create migration

---
+++ src/payload/migrations/20250226_071151.ts
@@ -0,0 +1,115 @@
+import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-sqlite'
+
+export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
+  await db.run(sql`CREATE TABLE \`media\` (
+  	\`id\` integer PRIMARY KEY NOT NULL,
+  	\`alt\` text NOT NULL,
+  	\`caption\` text,
+  	\`updated_at\` text DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ', 'now')) NOT NULL,
+  	\`created_at\` text DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ', 'now')) NOT NULL,
+  	\`url\` text,
+  	\`thumbnail_u_r_l\` text,
+  	\`filename\` text,
+  	\`mime_type\` text,
+  	\`filesize\` numeric,
+  	\`width\` numeric,
+  	\`height\` numeric,
+  	\`focal_x\` numeric,
+  	\`focal_y\` numeric,
+  	\`sizes_thumbnail_url\` text,
+  	\`sizes_thumbnail_width\` numeric,
+  	\`sizes_thumbnail_height\` numeric,
+  	\`sizes_thumbnail_mime_type\` text,
+  	\`sizes_thumbnail_filesize\` numeric,
+  	\`sizes_thumbnail_filename\` text,
+  	\`sizes_square_url\` text,
+  	\`sizes_square_width\` numeric,
+  	\`sizes_square_height\` numeric,
+  	\`sizes_square_mime_type\` text,
+  	\`sizes_square_filesize\` numeric,
+  	\`sizes_square_filename\` text,
+  	\`sizes_small_url\` text,
+  	\`sizes_small_width\` numeric,
+  	\`sizes_small_height\` numeric,
+  	\`sizes_small_mime_type\` text,
+  	\`sizes_small_filesize\` numeric,
+  	\`sizes_small_filename\` text,
+  	\`sizes_medium_url\` text,
+  	\`sizes_medium_width\` numeric,
+  	\`sizes_medium_height\` numeric,
+  	\`sizes_medium_mime_type\` text,
+  	\`sizes_medium_filesize\` numeric,
+  	\`sizes_medium_filename\` text,
+  	\`sizes_large_url\` text,
+  	\`sizes_large_width\` numeric,
+  	\`sizes_large_height\` numeric,
+  	\`sizes_large_mime_type\` text,
+  	\`sizes_large_filesize\` numeric,
+  	\`sizes_large_filename\` text,
+  	\`sizes_xlarge_url\` text,
+  	\`sizes_xlarge_width\` numeric,
+  	\`sizes_xlarge_height\` numeric,
+  	\`sizes_xlarge_mime_type\` text,
+  	\`sizes_xlarge_filesize\` numeric,
+  	\`sizes_xlarge_filename\` text,
+  	\`sizes_og_url\` text,
+  	\`sizes_og_width\` numeric,
+  	\`sizes_og_height\` numeric,
+  	\`sizes_og_mime_type\` text,
+  	\`sizes_og_filesize\` numeric,
+  	\`sizes_og_filename\` text
+  );
+  `)
+  await db.run(sql`CREATE INDEX \`media_updated_at_idx\` ON \`media\` (\`updated_at\`);`)
+  await db.run(sql`CREATE INDEX \`media_created_at_idx\` ON \`media\` (\`created_at\`);`)
+  await db.run(sql`CREATE UNIQUE INDEX \`media_filename_idx\` ON \`media\` (\`filename\`);`)
+  await db.run(sql`CREATE INDEX \`media_sizes_thumbnail_sizes_thumbnail_filename_idx\` ON \`media\` (\`sizes_thumbnail_filename\`);`)
+  await db.run(sql`CREATE INDEX \`media_sizes_square_sizes_square_filename_idx\` ON \`media\` (\`sizes_square_filename\`);`)
+  await db.run(sql`CREATE INDEX \`media_sizes_small_sizes_small_filename_idx\` ON \`media\` (\`sizes_small_filename\`);`)
+  await db.run(sql`CREATE INDEX \`media_sizes_medium_sizes_medium_filename_idx\` ON \`media\` (\`sizes_medium_filename\`);`)
+  await db.run(sql`CREATE INDEX \`media_sizes_large_sizes_large_filename_idx\` ON \`media\` (\`sizes_large_filename\`);`)
+  await db.run(sql`CREATE INDEX \`media_sizes_xlarge_sizes_xlarge_filename_idx\` ON \`media\` (\`sizes_xlarge_filename\`);`)
+  await db.run(sql`CREATE INDEX \`media_sizes_og_sizes_og_filename_idx\` ON \`media\` (\`sizes_og_filename\`);`)
+  await db.run(sql`CREATE TABLE \`posts\` (
+  	\`id\` integer PRIMARY KEY NOT NULL,
+  	\`hero_image_id\` integer,
+  	\`title\` text NOT NULL,
+  	\`content\` text NOT NULL,
+  	\`published_at\` text,
+  	\`updated_at\` text DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ', 'now')) NOT NULL,
+  	\`created_at\` text DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ', 'now')) NOT NULL,
+  	FOREIGN KEY (\`hero_image_id\`) REFERENCES \`media\`(\`id\`) ON UPDATE no action ON DELETE set null
+  );
+  `)
+  await db.run(sql`CREATE INDEX \`posts_hero_image_idx\` ON \`posts\` (\`hero_image_id\`);`)
+  await db.run(sql`CREATE INDEX \`posts_updated_at_idx\` ON \`posts\` (\`updated_at\`);`)
+  await db.run(sql`CREATE INDEX \`posts_created_at_idx\` ON \`posts\` (\`created_at\`);`)
+  await db.run(sql`ALTER TABLE \`payload_locked_documents_rels\` ADD \`media_id\` integer REFERENCES media(id);`)
+  await db.run(sql`ALTER TABLE \`payload_locked_documents_rels\` ADD \`posts_id\` integer REFERENCES posts(id);`)
+  await db.run(sql`CREATE INDEX \`payload_locked_documents_rels_media_id_idx\` ON \`payload_locked_documents_rels\` (\`media_id\`);`)
+  await db.run(sql`CREATE INDEX \`payload_locked_documents_rels_posts_id_idx\` ON \`payload_locked_documents_rels\` (\`posts_id\`);`)
+}
+
+export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
+  await db.run(sql`DROP TABLE \`media\`;`)
+  await db.run(sql`DROP TABLE \`posts\`;`)
+  await db.run(sql`PRAGMA foreign_keys=OFF;`)
+  await db.run(sql`CREATE TABLE \`__new_payload_locked_documents_rels\` (
+  	\`id\` integer PRIMARY KEY NOT NULL,
+  	\`order\` integer,
+  	\`parent_id\` integer NOT NULL,
+  	\`path\` text NOT NULL,
+  	\`users_id\` integer,
+  	FOREIGN KEY (\`parent_id\`) REFERENCES \`payload_locked_documents\`(\`id\`) ON UPDATE no action ON DELETE cascade,
+  	FOREIGN KEY (\`users_id\`) REFERENCES \`users\`(\`id\`) ON UPDATE no action ON DELETE cascade
+  );
+  `)
+  await db.run(sql`INSERT INTO \`__new_payload_locked_documents_rels\`("id", "order", "parent_id", "path", "users_id") SELECT "id", "order", "parent_id", "path", "users_id" FROM \`payload_locked_documents_rels\`;`)
+  await db.run(sql`DROP TABLE \`payload_locked_documents_rels\`;`)
+  await db.run(sql`ALTER TABLE \`__new_payload_locked_documents_rels\` RENAME TO \`payload_locked_documents_rels\`;`)
+  await db.run(sql`PRAGMA foreign_keys=ON;`)
+  await db.run(sql`CREATE INDEX \`payload_locked_documents_rels_order_idx\` ON \`payload_locked_documents_rels\` (\`order\`);`)
+  await db.run(sql`CREATE INDEX \`payload_locked_documents_rels_parent_idx\` ON \`payload_locked_documents_rels\` (\`parent_id\`);`)
+  await db.run(sql`CREATE INDEX \`payload_locked_documents_rels_path_idx\` ON \`payload_locked_documents_rels\` (\`path\`);`)
+  await db.run(sql`CREATE INDEX \`payload_locked_documents_rels_users_id_idx\` ON \`payload_locked_documents_rels\` (\`users_id\`);`)
+}
--- src/payload/migrations/index.ts
+++ src/payload/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250218_030524_initial from './20250218_030524_initial';
+import * as migration_20250226_071151 from './20250226_071151';
 
 export const migrations = [
   {
     up: migration_20250218_030524_initial.up,
     down: migration_20250218_030524_initial.down,
-    name: '20250218_030524_initial'
+    name: '20250218_030524_initial',
+  },
+  {
+    up: migration_20250226_071151.up,
+    down: migration_20250226_071151.down,
+    name: '20250226_071151'
   },
 ];
