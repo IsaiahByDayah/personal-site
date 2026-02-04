39b9e4e75e997132d7fc54096abdba3d4ce1a6d6
Isaiah Smith <jordankid93@gmail.com>
2026-02-03 17:53:57 -0800

    chore: remove plausible

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,11 @@
 import { PrismicPreview } from "@prismicio/next"
 import { Metadata } from "next"
-import PlausibleProvider from "next-plausible"
 import { Nunito_Sans } from "next/font/google"
 import { type ReactNode } from "react"
 
 import { Footer } from "@/app/_components/Footer"
 import { Header } from "@/app/_components/Header"
 import { TanstackProvider } from "@/app/_components/TanstackProvider"
-import { CUSTOM_DOMAIN, DOMAIN } from "@/lib/plausible"
 import { repositoryName } from "@/lib/prismicio"
 
 import "./globals.css"
@@ -38,20 +36,14 @@ const RootLayout = ({ children }: RootLayoutProps) => {
     <html lang="en" className={nunitoSans.variable}>
       <body className="text-jet-700">
         <TanstackProvider>
-          <PlausibleProvider
-            customDomain={CUSTOM_DOMAIN}
-            domain={DOMAIN}
-            trackOutboundLinks
-            selfHosted
-          >
-            <div className="px-2">
-              <Header className="border-b-jet-500/50 m-auto w-full max-w-5xl border-b py-2" />
-            </div>
-            {children}
-            <div className="bg-jet-500 light-on-dark px-2">
-              <Footer className="m-auto w-full max-w-5xl py-2" />
-            </div>
-          </PlausibleProvider>
+          <div className="px-2">
+            <Header className="border-b-jet-500/50 m-auto w-full max-w-5xl border-b py-2" />
+          </div>
+          {children}
+          <div className="bg-jet-500 light-on-dark px-2">
+            <Footer className="m-auto w-full max-w-5xl py-2" />
+          </div>
+
           <PrismicPreview repositoryName={repositoryName} />
         </TanstackProvider>
       </body>
